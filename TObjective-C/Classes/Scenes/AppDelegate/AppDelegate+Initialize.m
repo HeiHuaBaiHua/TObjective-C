6eadd0b567cf62ae920f860f41d07d9599915180
leihaiyin <leihaiyin@HeiHuaBaiHuaWIFI.local>
2018-02-08 15:15:27 +0800

    添加golang本地服务器

--- TObjective-C/Classes/Scenes/AppDelegate/AppDelegate+Initialize.m
+++ TObjective-C/Classes/Scenes/AppDelegate/AppDelegate+Initialize.m
@@ -20,7 +20,7 @@
     [self startNetworkMonitor];
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         
-//        [self setupSocket];
+        [self setupSocket];
 //        [self setupDatabase];
     });
 }
